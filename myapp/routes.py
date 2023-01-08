f870083bf7776c2fcdbd42771a72cc3aab35ba4d
antopiaz <mzheng.012@gmail.com>
2023-01-07 19:10:33 -0700

    uncommented the handler

--- myapp/routes.py
+++ myapp/routes.py
@@ -9,7 +9,7 @@ def setup_routes(app):
     #app.router.add_view('/', AddUserView)
     #app.router.add_get('/', hello)
     #app.router.add_post('/post',do_login)
-    #app.router.add_get('/', handler)
+    app.router.add_get('/', handler)
     app.router.add_get('/home', home, name='home')
     app.router.add_post('/home', home, name='home')
     app.router.add_get('/index', index, name='index')
