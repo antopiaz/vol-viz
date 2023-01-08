072be20030119c84d1bff7f1752769256d7b43c0
antopiaz <mzheng.012@gmail.com>
2023-01-07 17:24:22 -0700

    changed login to home

--- myapp/routes.py
+++ myapp/routes.py
@@ -10,8 +10,8 @@ def setup_routes(app):
     #app.router.add_get('/', hello)
     #app.router.add_post('/post',do_login)
     app.router.add_get('/', handler)
-    app.router.add_get('/login', login, name='login')
-    app.router.add_post('/login', login, name='login')
+    app.router.add_get('/home', login, name='home')
+    app.router.add_post('/home', login, name='home')
     app.router.add_get('/index', index, name='index')
     setup_static_routes(app)
 
