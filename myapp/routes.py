1030db5fb7917dfa592c7ce97b6fc28ea75b8dd0
antopiaz <mzheng.012@gmail.com>
2023-01-07 17:30:46 -0700

    change login again 2

--- myapp/routes.py
+++ myapp/routes.py
@@ -9,9 +9,9 @@ def setup_routes(app):
     #app.router.add_view('/', AddUserView)
     #app.router.add_get('/', hello)
     #app.router.add_post('/post',do_login)
-    app.router.add_get('/', handler)
-    app.router.add_get('/home', login, name='home')
-    app.router.add_post('/home', login, name='home')
+    #app.router.add_get('/', handler)
+    app.router.add_get('/home', login, name='login')
+    app.router.add_post('/home', login, name='login')
     app.router.add_get('/index', index, name='index')
     setup_static_routes(app)
 
