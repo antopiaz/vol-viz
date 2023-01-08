fb15d40daaa60433c56fdafa3c4597d25e430c80
antopiaz <mzheng.012@gmail.com>
2023-01-08 10:31:32 -0700

    check1

--- myapp/routes.py
+++ myapp/routes.py
@@ -11,7 +11,7 @@ def setup_routes(app):
     #app.router.add_post('/post',do_login)
     app.router.add_get('/', handler)
     app.router.add_get('/home', home, name='home')
-    app.router.add_post('/home', home, name='display')
+    app.router.add_post('/home', home, name='home')
     app.router.add_get('/index', index, name='index')
     setup_static_routes(app)
 
